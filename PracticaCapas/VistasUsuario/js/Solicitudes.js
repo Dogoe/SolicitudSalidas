eecb874a1547d6ce99cdfcf39ae6f33f52f2aaf0
Josue <josue.jimenez@uabc.edu.mx>
2017-12-20 02:01:35 -0800

    algoo

--- PracticaCapas/VistasUsuario/js/Solicitudes.js
+++ PracticaCapas/VistasUsuario/js/Solicitudes.js
@@ -2,18 +2,10 @@
     $('[data-toggle="tooltip"]').tooltip();
 
     var tabSeleccionada = $('#ContentPlaceHolder_TABSeleccionada').val();
-    //console.log(algo);
     $('#tabSolicitudesPendientes a[href="#' + tabSeleccionada + '"]').tab('show');
-    //$('#tabHistorialSolicitudes a[href="#' + algo + '"]').tab('show');
-    /*$.notify({
-        // options
-        message: "Hello World"
-    }, {
-            // settings
-            type: "danger"
-        });*/
-   
-    
+
+
+
 });
 function openModalSolicitudes() {
     $('#ModalModificarConsultarSolicitud').modal('show');
@@ -25,30 +17,22 @@ function openModalConfirmar() {
     $('#ContentPlaceHolder_txtLugarEvento').removeAttr("required");
     $('#ContentPlaceHolder_txtFechaSalida').removeAttr("required");
     $('#ContentPlaceHolder_txtHoraSalida').removeAttr("required");
-    $('#ContentPlaceHolder_txtFechaLLegada').removeAttr("required");
-    $('#ContentPlaceHolder_txtHoraLlegada').removeAttr("required");
+    $('#ContentPlaceHolder_txtFechaRegreso').removeAttr("required");
+    $('#ContentPlaceHolder_txtHoraRegreso').removeAttr("required");
     $('#ContentPlaceHolder_txtRecursoSolicitadoOtro').removeAttr("required");
     $('#ContentPlaceHolder_txtCantPersonas').removeAttr("required");
     $('#ContentPlaceHolder_txtActividadOtros').removeAttr("required");
     //----------------------
-    
+
     $('#ModalConfirmarSolicitud').modal('show');
-    //$('#tabSolicitudesPendientes a[href="' + "#validador" + '"]').tab('show');
 }
 //------------------
-/*function abrirPDF(url) {
-    var a = document.createElement("a");
-    a.target = "_blank";
-    a.href = url;
-    a.click();
-    //window.open(url, "Diseño Web", "width=300, height=200");
-}*/
 function abrirPDF(url) {
     //debugger
     window.open(url, "_blank");
     //window.open(url, "Diseño Web", "width=300, height=200");
 }
-function Notificaciones(msj,color) {
+function Notificaciones(msj, color) {
     $.notify({
         // options
         icon: 'glyphicon glyphicon-warning-sign',
@@ -71,15 +55,3 @@ function Notificaciones(msj,color) {
             },
         });
 }
-//window.open("myurl/files/" + nombreArchivo + ".pdf", "_blank");
-/*$("body").on("click", "[id*=gvSolicitudesPendientesAdmin] .Aceptar", function () {
-    //-----------------------------------
-    var row = $(this).closest("tr");
-    var Id = row.find(".IdSolicitud").find("span").html();
-    console.log(Id);
-    //$('#ContentPlaceHolder_txtIdSolicitudHidde').val(Id);
-    $('#ModalAceptarSolicitud').modal('show');
-
-    //return false;
-});*/
-//--------------
